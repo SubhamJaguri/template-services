77a03d9e02c732a901216616b56359b8f0be2099
Subham Jaguri <sjaguri222@gmail.com>
2023-05-20 21:25:37 +0000

    feat(ci-cd): add expo sample app
    AB#0

---
+++ services/expo-sample-app/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
